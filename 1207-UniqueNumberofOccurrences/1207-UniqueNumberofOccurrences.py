739b4aa86c685c1c79b773926117c9e688f8bba7
Neenu025 <141194453+Neenu025@users.noreply.github.com>
2024-01-18 00:24:30 +0530

    Update file 1/18/2024, 12:24:29 AM

--- 1207-UniqueNumberofOccurrences/1207-UniqueNumberofOccurrences.py
+++ 1207-UniqueNumberofOccurrences/1207-UniqueNumberofOccurrences.py
@@ -1,4 +1,12 @@
 class Solution:
-    def uniqueOccurrences(self, arr: List[int]) -> bool:
-        
-[1,2,2,1,1,3]
+    def timeRequiredToBuy(self, tickets: list[int], k: int) -> int:
+        secs = 0 
+        i = 0
+        while tickets[k] != 0:
+            if tickets[i] != 0: 
+                tickets[i] -= 1 
+                secs += 1 
+            i = (i + 1) % len(tickets)
+            
+        return secs
+[
