7cf7478850351edb15c3428c676bcd40a21f4caf
Neenu025 <141194453+Neenu025@users.noreply.github.com>
2024-01-18 11:31:33 +0530

    Create file

---
+++ 2696-MinimumStringLengthAfterRemovingSubstrings/2696-MinimumStringLengthAfterRemovingSubstrings.py
@@ -0,0 +1,14 @@
+
+        while 'AB' in s or 'CD' in s:
+            s=s.replace('AB','')
+            
+
+            s=s.replace('CD','')
+        if 'AB' or 'CD' not in s: 
+            return len(s)
+            return len(s)
+    def minLength(self, s: str) -> int:
+class Solution:
+            continue
+        
+"
