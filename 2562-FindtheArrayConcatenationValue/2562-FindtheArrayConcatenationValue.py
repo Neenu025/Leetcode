160f4defde0a937afb72d2f4e51e92e912ea8c0f
Neenu025 <141194453+Neenu025@users.noreply.github.com>
2024-01-20 14:03:48 +0530

    Create file

---
+++ 2562-FindtheArrayConcatenationValue/2562-FindtheArrayConcatenationValue.py
@@ -0,0 +1,25 @@
+                result = a + b
+                print(result)
+                concat = int(result)
+                a = ""
+                b = str(nums.pop()) 
+            if len(nums) >0:
+        concat_value = 0
+        while len(nums) > 0:
+            a = str(nums.pop(0))
+    def findTheArrayConcVal(self, nums: List[int]) -> int:  
+        concat = 0  
+        if len(nums)==1:
+class Solution:
+            return nums[0]
+                b = ""
+                result = ""
+
+                concat_value += concat
+                concat = 0
+            if len(nums) == 1:
+                concat_value += nums[0]
+              
+            
+        return concat_value
+[
