e34e9a3e47fad48468d1a9082a18375941fb993c
Neenu025 <141194453+Neenu025@users.noreply.github.com>
2024-01-22 11:50:37 +0530

    Update file 1/22/2024, 11:50:37 AM

--- 2729-CheckifTheNumberisFascinating/2729-CheckifTheNumberisFascinating.py
+++ 2729-CheckifTheNumberisFascinating/2729-CheckifTheNumberisFascinating.py
@@ -1,11 +1,9 @@
-        #         return False
+class Solution:
+    def isFascinating(self, n: int) -> bool:
         new_n = str(n) + str(2 * n) + str(3 * n)
         for i in range(1, 10):
             if new_n.count(str(i)) != 1 or '0' in new_n:
                 return False
         return True
         
-        #     if new_n.count(str(i)) == 1 and '0' not in new_n:
-        #         return True
-        #     else:
 1
