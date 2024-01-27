dd7289b680bcf2389626426e50b926d92b9714ff
Neenu025 <141194453+Neenu025@users.noreply.github.com>
2024-01-27 21:59:43 +0530

    Create file

---
+++ Pythonmethodusingtwoloop/Pythonmethodusingtwoloop.py
@@ -0,0 +1,16 @@
+        
+                if num1 == num2:
+                    temp = max(temp, nums[i] + nums[j])
+        return -1 if temp == 0 else temp
+                num2 = max(str(nums[j]))
+            
+    
+              
+
+        for i in range(l-1):
+            num1 = max(str(nums[i]))
+            for j in range(i+1, l):
+    def maxSum(self, nums: List[int]) -> int:
+        temp = 0
+        l = len(nums)
+[
