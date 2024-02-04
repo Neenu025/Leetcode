71419c53b0678f16accef8c43776a9b81de4ecba
Neenu025 <141194453+Neenu025@users.noreply.github.com>
2024-02-04 23:53:25 +0530

    Update file 2/4/2024, 11:53:23 PM

--- 2788-SplitStringsbySeparator/2788-SplitStringsbySeparator.py
+++ 2788-SplitStringsbySeparator/2788-SplitStringsbySeparator.py
@@ -1,12 +1,15 @@
 class Solution:
     def splitWordsBySeparator(self, words: List[str], separator: str) -> List[str]:
+        result = []
         for word in words:
             split_word = word.split(separator)
+            print(split_word)
             for sub_word in split_word:
                 if sub_word != "":
                     result.append(sub_word)
-
-        result = []
         return result
 
+
+
+        
 [
