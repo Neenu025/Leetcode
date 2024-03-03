1ff6fb0f9dca7ca69830bdc3da0e3d65b3fbfb4a
Neenu025 <141194453+Neenu025@users.noreply.github.com>
2024-03-03 22:56:47 +0530

    Update file 3/3/2024, 10:56:46 PM

--- 455-AssignCookies/455-AssignCookies.py
+++ 455-AssignCookies/455-AssignCookies.py
@@ -1,10 +1,10 @@
+class Solution:
     def findContentChildren(self, g: List[int], s: List[int]) -> int:
         g.sort(reverse = True)
         s.sort(reverse = True)
         result, i , j = 0 ,0,0
         while i<len(g) and j<len(s):
             if g[i]<=s[j]:
-class Solution:
                 result+=1
                 i+=1
                 j+=1
