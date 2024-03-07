e87838752be8824ebd256a402f961fdf53d4e04b
Neenu025 <141194453+Neenu025@users.noreply.github.com>
2024-03-07 23:53:38 +0530

    Update file 3/7/2024, 11:53:35 PM

--- 859-BuddyStrings/859-BuddyStrings.py
+++ 859-BuddyStrings/859-BuddyStrings.py
@@ -4,13 +4,10 @@ class Solution:
         if len(s) != len(goal):
         
         if s == goal:
-            # If s and goal are the same, check if s contains duplicate characters
-            return len(set(s)) < len(s)
+           return len(set(s)) < len(s)
         
-        # Find the positions where the characters differ
         diffs = [(a, b) for a, b in zip(s, goal) if a != b]
         
-        # There should be exactly two differences, and they should be reversible
         return len(diffs) == 2 and diffs[0] == diffs[1][::-1]
 
 "
