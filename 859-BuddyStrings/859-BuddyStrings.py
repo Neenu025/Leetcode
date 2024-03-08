99cd2baac881d1049af6c8fdc52a639a15585a09
Neenu025 <141194453+Neenu025@users.noreply.github.com>
2024-03-08 23:59:33 +0530

    Update file 3/8/2024, 11:59:29 PM

--- 859-BuddyStrings/859-BuddyStrings.py
+++ 859-BuddyStrings/859-BuddyStrings.py
@@ -1,14 +1,8 @@
 class Solution:
-    def buddyStrings(self, s: str, goal: str) -> bool:
-            return False
-        if len(s) != len(goal):
+    def addDigits(self, num: int) -> int:
+        if num <=0:
+            return 0
+        return (num-1)%9 + 1
+
         
-        if s == goal:
-            return len(set(s)) < len(s)
-        
-        diffs = [(a, b) for a, b in zip(s, goal) if a != b]
-
-        return len(diffs) == 2 and diffs[0] == diffs[1][::-1]
-
-
-"
+3
