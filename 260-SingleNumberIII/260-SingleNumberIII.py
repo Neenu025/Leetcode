359b73891b648ed76559ec3715c9f3827cc15c08
Neenu025 <141194453+Neenu025@users.noreply.github.com>
2024-03-10 00:16:06 +0530

    Update file 3/10/2024, 12:16:02 AM

--- 260-SingleNumberIII/260-SingleNumberIII.py
+++ 260-SingleNumberIII/260-SingleNumberIII.py
@@ -1,9 +1,9 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> List[int]:
+        result=[]
         for i in nums:
             if nums.count(i) == 1:
                 result.append(i)
-        result=[]
         return result
                       
 [
