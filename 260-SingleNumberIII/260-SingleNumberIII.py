86852cce9dd12565f8e22cfa0adbcd69b714bc57
Neenu025 <141194453+Neenu025@users.noreply.github.com>
2024-03-10 23:46:37 +0530

    Update file 3/10/2024, 11:46:32 PM

--- 260-SingleNumberIII/260-SingleNumberIII.py
+++ 260-SingleNumberIII/260-SingleNumberIII.py
@@ -1,9 +1,5 @@
 class Solution:
-    def singleNumber(self, nums: List[int]) -> List[int]:
-        result=[]
-        for i in nums:
-            if nums.count(i) == 1:
-                result.append(i)
-        return result
-                      
-[
+    def minimizedStringLength(self, s: str) -> int:
+        return len(set(s))
+        
+"
