cd4da964689bd8d7f12ec073790e5b7bbb30eb38
Neenu025 <141194453+Neenu025@users.noreply.github.com>
2024-03-15 00:00:52 +0530

    Update file 3/15/2024, 12:00:50 AM

--- 151-ReverseWordsinaString/151-ReverseWordsinaString.py
+++ 151-ReverseWordsinaString/151-ReverseWordsinaString.py
@@ -1,16 +1,16 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
-        
         s_list = s.split()
-
         s_poped=[]
 
 
-
+        while s_list:
             s_pop = s_list.pop()
             s_poped.append(s_pop)
         return ' '.join(s_poped)
 
-        while s_list:
+
+        
+
         
 "
