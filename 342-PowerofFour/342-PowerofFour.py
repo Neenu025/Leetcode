c8648c1d4f7d98a6c27b09c96e88e7fff0ae8051
Neenu025 <141194453+Neenu025@users.noreply.github.com>
2024-04-03 15:22:38 +0530

    Create file

---
+++ 342-PowerofFour/342-PowerofFour.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isPowerOfFour(self, n: int) -> bool:
+       
+        for i in range(16):
+            power_of_four = 4**i
+
+            if power_of_four == n:
+                return True
+           
+        return False
+        
+        
+1
