1df7ffe4958e50673704f6fdefd5e7dc4613cf3c
Neenu025 <141194453+Neenu025@users.noreply.github.com>
2024-04-06 00:52:23 +0530

    Update file 4/6/2024, 12:52:18 AM

--- 344-ReverseString/344-ReverseString.py
+++ 344-ReverseString/344-ReverseString.py
@@ -1,9 +1,20 @@
 class Solution:
-    def reverseString(self, s: List[str]) -> None:
-        """
-        Do not return anything, modify s in-place instead.
-        """
-        s[:] = s[::-1]
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # output =[]
+        # diff = 0
+        # for i in range(len(nums)):
+        #     diff = target - nums[i]
+        #     if diff in nums and i!=nums.index(diff):
+        #         output.append(i)
+        #         output.append(nums.index(diff))
+        #         break
+        # return output
+
+
+
         
-        
-[
+        num_map = {}
+        for i, num in enumerate(nums):
+            difference = target - num
+            if difference in num_map:
+[2,7,11,15]
