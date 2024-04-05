179480a49a1b8d694b8f9c5c56f43995bb933f9e
Neenu025 <141194453+Neenu025@users.noreply.github.com>
2024-04-05 23:46:11 +0530

    Update file 4/5/2024, 11:46:06 PM

--- 1-TwoSum/1-TwoSum.py
+++ 1-TwoSum/1-TwoSum.py
@@ -1,3 +1,14 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # output =[]
+        # diff = 0
+        # for i in range(len(nums)):
+        #     diff = target - nums[i]
+        #     if diff in nums and i!=nums.index(diff):
+        #         output.append(i)
+        #         output.append(nums.index(diff))
+        #         break
+        # return output
 
 
 
@@ -6,10 +17,4 @@
         for i, num in enumerate(nums):
             difference = target - num
             if difference in num_map:
-                return [num_map[difference],i]
-            num_map[num] = i
-        return []
-
-
-        
-[2,7,11,15]
+[
