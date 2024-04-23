71c3efa31d6a206f97ea51bd789e8e927c10f217
Neenu025 <141194453+Neenu025@users.noreply.github.com>
2024-04-23 09:31:56 +0530

    Update file 4/23/2024, 9:31:55 AM

--- 645-SetMismatch/645-SetMismatch.py
+++ 645-SetMismatch/645-SetMismatch.py
@@ -1,11 +1,11 @@
+
+class Solution:
     def findErrorNums(self, nums: List[int]) -> List[int]:
         L = len(nums)
         op = []
         nums_org = list(range(1, L + 1))
        
         duplicate = sum(nums) - sum(set(nums))
-class Solution:
-
         missing = sum(nums_org) - sum(set(nums))
 
         op.append(duplicate)
