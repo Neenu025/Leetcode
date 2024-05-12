e5510ebab31bcf916a692deed3cabfbcf65d58ba
Neenu025 <141194453+Neenu025@users.noreply.github.com>
2024-05-12 23:51:04 +0530

    Update file 5/12/2024, 11:51:01 PM

--- 367-ValidPerfectSquare/367-ValidPerfectSquare.py
+++ 367-ValidPerfectSquare/367-ValidPerfectSquare.py
@@ -10,11 +10,11 @@
         return False
 
 
-
-
-        
         #         return True
         # return False
 
         # Time out error
+    def isPerfectSquare(self, num: int) -> bool:
+        # for i in range(1,num+1):
+        #     if i*i == num:
 1
