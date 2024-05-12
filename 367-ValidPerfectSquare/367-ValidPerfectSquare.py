46f3f567d6eba3e31051b4a3eedb3c9f2dc81d63
Neenu025 <141194453+Neenu025@users.noreply.github.com>
2024-05-12 23:44:15 +0530

    Create file

---
+++ 367-ValidPerfectSquare/367-ValidPerfectSquare.py
@@ -0,0 +1,20 @@
+        left, right = 1, num
+        while left <= right:
+            mid = (left + right) // 2
+            if mid * mid == num:
+                return True
+            elif mid * mid < num:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return False
+
+
+
+
+        
+        #         return True
+        # return False
+
+        # Time out error
+1
